55f41997248734cc2262b8158d4ca4078147b928
Gaganyadavv <144685716+Gaganyadavv@users.noreply.github.com>
2023-11-06 18:15:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,132 @@
-
+import React, { useEffect, useState } from 'react'; 
 import './App.css';
-import React from 'react';
+import abhinayImage from "./abhinay.jpeg"; 
+
 function App() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [showTopButton, setShowTopButton] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setMenuOpen(false);
+      if (window.scrollY > 0) {
+        setShowTopButton(true);
+      } else {
+        setShowTopButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    handleScroll(); 
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  
+  const handleLinkClick = (e, target) => {
+    e.preventDefault();
+    const targetElement = document.querySelector(target);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
-    <div className="portfolio">
-      <header className="header">
-        <h1>Gagan Yadav (21BCE9485)</h1>
+    <div className="App">
+      <header>
+        <div className="user">
+          <img src={abhinayImage} alt="abhinay" />
+          <h3 className="name">abhinay</h3>
+        </div>
+        <nav className="navbar">
+          <ul className="nav">
+            <li className='navlink'><a href="#home">HOME</a></li>
+            <li className='navlink'><a href="#about">ABOUT</a></li>
+            <li className='navlink'><a href="#education">EDUCATION</a></li>
+            <li className='navlink'><a href="#contact">CONTACT</a></li>
+          </ul>
+        </nav>
       </header>
 
-      <div className="content">
-        <section className="section">
-          <h2 style={{textAlign: "center"}}>INTODUCTION</h2>
-          <br/>
-          <p>
-            I'm Gagan Yadav, a B.Tech student with a passion for Artificial Intelligence and Machine Learning,from VIT-AP
-          </p>
-        </section>
+      <div id="menu" className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'}`} onClick={handleMenuClick}></div>
 
-        <section className="section">
-          <h2 style={{textAlign: "center"}}>SKILLS</h2>
-          <br/>
-          <ul>
-            <li>Java</li>
-            <li>Python</li>
-            <li>Data Structures</li>
-            <li>HTML,CSS,JavaScript,React.js,Node.js</li>
-            <li>SQL,MongoDb</li>
-          </ul>
-        </section>
+      <section className="home" id="home">
+        <h3>HELLO!!</h3>
+        <h1>This is <span>Gagan yadav</span></h1>
+      </section>
 
-        <section className="section">
-          <h2 style={{textAlign: "center"}}>PROJECTS</h2>
-          <br/>
-          <ul>
-            <li>Disease Diagnosis Through Multi-Modal Data Integration and Data Weighting Using Deep Learning</li>
-            <li>CrimeBot: Crime Registration and Awareness Web Application</li>
-          </ul>
-        </section>
-        <section className="section">
-          <h2 style={{textAlign: "center"}}>CERTIFICATIONS</h2>
-          <br/>
-          <ul>
-            <li>AWS Academy Graduate - AWS Academy Cloud Foundations</li>
-            <li>React - The Complete Guide 2023 (incl. React Router & Redux)</li>
-          </ul>
-        </section>
-        
+      <section className="about" id="about">
+        <h1 className="heading"> <span>about</span> me </h1>
+        <div className="row">
+          <div className="info">
+            <h3> <span> Name : </span> Gagan Yadav</h3>
+            <h3> <span> Qualification : </span> B.Tech(pursuing) </h3>
+            <h3> <span> Address</span> Guntur</h3>
+            <h3> <span> Strengths : </span> Honesty, Patience, Dedication </h3>
+            <h3> <span> Hobbies: </span> Reading Books, Travelling </h3>
+            <h3> <span> Language : </span> Telugu, Hindi, English </h3>
+          </div>
+        </div>
+      </section>
 
-        <section className="section">
-          <h2 style={{textAlign: "center"}}>CONTACT</h2>
-          <br/>
-          <p>Phone: +91 0987654321</p>
-          <a href="mailto:gaganyadav.21bce9485@vitapstudent.ac.in">Email me</a>
-        </section>
-      </div>
+      <section className="education" id="education">
+        <h1 className="heading"> Education <span>Details</span> </h1>
+        <div className="box-container">
+          <div className="box">
+            <i className="fas fa-graduation-cap"></i>
+            <span>2018-2019</span>
+            <h3>SSC</h3>
+            <p style={{ fontSize: '20px' }}>Sri Chaitanya</p>
+          </div>
+          <div className="box">
+            <i className="fas fa-graduation-cap"></i>
+            <span>2019-2021</span>
+            <h3>Intermediate</h3>
+            <h3>MPC</h3>
+            <p style={{ fontSize: '20px' }}>Sri chaitanya
+            </p>
+          </div>
+          <div className="box">
+            <i className="fas fa-graduation-cap"></i>
+            <span>2021-2025</span>
+            <h3>B.Tech</h3>
+            <h4> </h4>
+            <p style={{ fontSize: '20px' }}>VIT AP University</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="contact" id="contact">
+        <h1 className="heading"> <span>contact</span> me </h1>
+        <div className="row">
+          <div className="content">
+            <h3 className="title">contact info</h3>
+            <div className="info">
+              <h3> <i className="fas fa-envelope"></i> gaganyadavgajjala@gmail.com</h3>
+              <h3> <i className="fas fa-phone"></i> 7075251223 </h3>
+              <h3> <i className="fas fa-map-marker-alt"></i> Kadapa </h3>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {showTopButton && (
+        <a href="#home" className="top" onClick={(e) => handleLinkClick(e, '#home')}>
+          <img src={abhinayImage} alt="" />
+        </a>
+      )}
     </div>
   );
 }
 
-export default App; // Add this export statement to make the component available for use in other parts of your application
+export default App;
